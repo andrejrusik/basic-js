0ce07348a874cb5830dc7571d5de9d263293b894
Andrei Rushnitski <andrejrusik@gmail.com>
2022-04-25 15:43:23 +0200

    feat: add decision counts-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,9 +1,9 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix where you have to find cats by ears "^^"
  *
- * @param {Array<Array>} matrix 
+ * @param {Array<Array>} matrix
  * @return {Number} count of cats found
  *
  * @example
@@ -14,11 +14,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * ]) => 3`
  *
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function countCats(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  let counter = 0;
+
+  for (const arr of matrix) {
+    arr.forEach((el) => {
+      if (el === "^^") {
+        counter++;
+      }
+    });
+  }
+
+  return counter;
 }
 
 module.exports = {
-  countCats
+  countCats,
 };
