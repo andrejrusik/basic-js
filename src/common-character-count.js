f6ea0215e024561242a7507ff21669f0a2dad473
Andrei Rushnitski <andrejrusik@gmail.com>
2022-04-25 17:29:50 +0200

    feat: add decision commom-character-count

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given two strings, find the number of common characters between them.
@@ -11,11 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+
+function getCommonCharacterCount(s1, s2) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+
+  const s1Arr = s1.split("");
+  const s2Arr = s2.split("");
+  let counter = 0;
+
+  for (const elS1 of s1Arr) {
+    if (s2Arr.indexOf(elS1) !== -1) {
+      counter++;
+      s2Arr.splice(s2Arr.indexOf(elS1), 1);
+    }
+  }
+  return counter;
 }
 
 module.exports = {
-  getCommonCharacterCount
+  getCommonCharacterCount,
 };
