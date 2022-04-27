b0ea5e50b14c9e353d734f4834b339303250deb8
Andrei Rushnitski <andrejrusik@gmail.com>
2022-04-27 20:59:00 +0200

    feat: add decision file-names

--- src/file-names.js
+++ src/file-names.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * There's a list of file, since two files cannot have equal names,
@@ -15,11 +15,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
+function renameFiles(names) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let useSuffix = {};
+  for (let i = 0; i < names.length; i++) {
+    for (let j = i + 1; j < names.length; j++) {
+      if (names[i] === names[j]) {
+        useSuffix.hasOwnProperty(names[i])? useSuffix[names[i]]++ : useSuffix[names[i]] = 1;
+        names[j] = names[i] + `(${useSuffix[names[i]]})`;
+      }
+    }
+  }
+  console.log(names);
+  return names
 }
 
+// renameFiles(["file", "file", "image", "file(1)", "file"])
+
 module.exports = {
-  renameFiles
+  renameFiles,
 };
