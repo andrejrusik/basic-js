352206f726217c390181313ad0847ea46cb0f40c
Andrei Rushnitski <andrejrusik@gmail.com>
2022-05-01 18:34:01 +0200

    feat: add decision simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,65 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
+  chains: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this.chains.length;
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value = "") {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.chains.push(`( ${value} )`);
+    return this;
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+
+    if (
+      position <= 0 ||
+      position > this.getLength() ||
+      isNaN(position) ||
+      !Number.isInteger(position)
+    ) {
+      this.chains.length = 0;
+      throw new Error("You can't remove incorrect link!");
+    }
+
+    this.chains.splice(position - 1, 1);
+    return this;
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    this.chains.reverse();
+    return this;
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here;
+    const result = this.chains.join(`~~`);
+    this.chains.length = 0;
+    return result;
+  },
 };
 
+// const result = chainMaker
+//   .addLink(1)
+//   .addLink(2)
+//   .reverseChain()
+//   .addLink()
+//   .addLink(4)
+//   .removeLink(2.1)
+//   .addLink(6)
+//   .getLength();
+// // .finishChain();
+// console.log(result);
 module.exports = {
-  chainMaker
+  chainMaker,
 };
