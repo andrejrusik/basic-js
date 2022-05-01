26f487afe7e60b07008505f2b8139592a6a3a43e
Andrei Rushnitski <andrejrusik@gmail.com>
2022-05-02 01:25:20 +0200

    feat: add decision vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,99 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direct = true) {
+    this.direct = direct;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
+
+  encrypt(text, code) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    if (!text || !code) {
+      throw Error("Incorrect arguments!");
+    }
+    const textUp = text.toUpperCase();
+    const codeUp = code.toUpperCase();
+    let res = "";
+    let counter = 0;
+
+    for (let i = 0; i < textUp.length; i++) {
+      if (textUp[i].charCodeAt() < 65 || textUp[i].charCodeAt() > 90) {
+        res += textUp[i];
+      } else {
+        res += String.fromCharCode(
+          ((textUp[i].charCodeAt() +
+            codeUp[counter % codeUp.length].charCodeAt()) %
+            26) +
+            65
+        );
+        counter++;
+      }
+    }
+
+    return this.direct === true || this.direct === undefined
+      ? res
+      : res.split("").reverse().join("");
+  }
+  decrypt(text, code) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+
+    if (!text || !code) {
+      throw Error("Incorrect arguments!");
+    }
+    const textUp = text.toUpperCase();
+    const codeUp = code.toUpperCase();
+    let res = "";
+    let counter = 0;
+
+    for (let i = 0; i < textUp.length; i++) {
+      if (textUp[i].charCodeAt() < 65 || textUp[i].charCodeAt() > 90) {
+        res += textUp[i];
+      } else {
+        res += String.fromCharCode(
+          ((textUp[i].charCodeAt() -
+            codeUp[counter % codeUp.length].charCodeAt() <
+          0
+            ? textUp[i].charCodeAt() -
+              codeUp[counter % codeUp.length].charCodeAt() +
+              26
+            : textUp[i].charCodeAt() -
+              codeUp[counter % codeUp.length].charCodeAt()) %
+            26) +
+            65
+        );
+        counter++;
+      }
+    }
+    console.log(res);
+    return this.direct === true || this.direct === undefined
+      ? res
+      : res.split("").reverse().join("");
   }
 }
-
+// console.log(String.fromCharCode(65));
+// const directMachine = new VigenereCipheringMachine();
+// directMachine.encrypt("attack at dawn!", "alphonse");
+// directMachine.decrypt("AEIHQX SX DLLU!", "alphonse");
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
