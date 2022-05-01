2a7025dea9c3302999e9e0c028848053360e9758
Andrei Rushnitski <andrejrusik@gmail.com>
2022-05-01 15:48:28 +0200

    feat: add decision matrix-elements-sum

--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix, a rectangular matrix of integers,
@@ -16,11 +16,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
+function getMatrixElementsSum(matrix) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let res = matrix[0].reduce((a, b) => a+b, 0);
+  for (let i = 0; i < matrix.length - 1; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] !== 0) {
+        res += matrix[i + 1][j]
+      }
+    }
+  }
+  console.log(res);
+  return res
 }
-
+// getMatrixElementsSum([
+//   [0, 1, 1, 2],
+//   [0, 5, 0, 0],
+//   [2, 0, 3, 3],
+// ]);
 module.exports = {
-  getMatrixElementsSum
+  getMatrixElementsSum,
 };
